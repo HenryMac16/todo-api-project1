9165e79c192ee517085a67b4a15e1d92b0619f8f
Henry Macarthur <henrymacarthur@s-169-232-98-83.resnet.ucla.edu>
2019-01-13 17:36:53 -0800

    ADD Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -29,6 +30,34 @@ app.get('/todos', (req, res) => {
   });
 });
 //this callback fires when app runs
+
+//GET /todos/ID-make it dynamic
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+      res.status(200).send({todo}); //todo:todo
+  }).catch((e) => {
+    res.status(404).send();
+  });
+  //validate id using isValid
+    //stop function execution, respond with 404, empty send
+  // findById, query todo
+    //success
+      //if todo - send it back
+      //else - send back 404, empty body
+    //error
+      // 400 - send empty body back
+
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000')
 });
