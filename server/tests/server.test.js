9363f4add2024371fa1a116401064a1feac3eec0
Henry Macarthur <henrymacarthur@s-169-232-127-246.resnet.ucla.edu>
2019-01-14 21:26:16 -0800

    add tests for patch /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 1000
 }];
 
 beforeEach((done) => {
@@ -137,5 +139,46 @@ describe('DELETE /todos/:id', () => {
       .delete('/todos/123abc')
       .expect(404)
       .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+
+  it('should remove a todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'new text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .expect(200)
+      .send({
+        completed: true,
+        text: text
+      })
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done)
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    var text = 'this is the new text!'
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .expect(200)
+      .send({
+        completed: false,
+        text
+      })
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.completedAt).toNotExist;
+      })
+      .end(done)
   });
 });
